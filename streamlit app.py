d933d9ee67ccc0b8d2a71fcc1a190b918c1ee07b
KiranKhurshid <kirankhurshid42@gmail.com>
2024-09-04 12:33:56 +0000

    Added streamlit.app

--- streamlit app.py
+++ streamlit app.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import joblib
-import sklearn.metrics import accuracy
-
+# from sklearn.metrics import accuracy
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from streamlit as 
-model = joblib.load("livemodelV1.pkl")
-data = pd.read_csv("mobile_price_range_data (1)")
+import streamlit as st
+model = joblib.load("liveModelV1.pk1")
+data = pd.read_csv("mobile_price_range_data (1).csv")
 
 X = data.iloc[:,:-1]
 Y= data.iloc[:,-1]
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size =) 
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size =0.2) 
 
 #make prediction for X_test set
 y_pred = model.predict(X_test)
@@ -18,14 +18,14 @@ y_pred = model.predict(X_test)
 #calculate accuracy
 accuracy = accuracy_score(y_test,y_pred )
 
-st.title("Model accuracy and reall_time prediction")
+st.title("Model accuracy and real_time prediction")
 #display acuracy
 st.write(f"Model{accuracy}")
 #real time prediction based on user inputs
-st.header("Real time prediction")
+st.header("Real-Time Prediction")
 input_data= []
 for col in X_test.columns:
-     input_value =st.number_input(f'Input for features{col}',value=)
+     input_value =st.number_input(f'Input for features{col}',value=0.0)
      input_data.append(input_value)
 
 #convert input data to dataframe 
